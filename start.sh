c8cf91adffc494acd2c7d019bd1fe618972f1396
Sarthak Kale <83166658+sarthakkale16@users.noreply.github.com>
2022-12-26 03:00:21 +0530

    Add files via upload

--- start.sh
+++ start.sh
@@ -1,12 +1,4 @@
-if [ -z $UPSTREAM_REPO ]
-then
-  echo "Cloning main Repository"
-  git clone https://github.com/sarthakkale16/aadicode2.git /EvaMaria
-else
-  echo "Cloning Custom Repo from $UPSTREAM_REPO "
-  git clone $UPSTREAM_REPO /aadicode2
-fi
-cd /aadicode2
-pip3 install -U -r requirements.txt
-echo "Starting Bot...."
-python3 bot.py
+#!/bin/bash
+cd /app
+echo "----- Now deployed web booting your repo ------ " 
+gunicorn -b :5000 --reload --access-logfile - --error-logfile - app:app
